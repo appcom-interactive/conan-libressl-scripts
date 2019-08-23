34c0953b93a4a5617f85d8cd9358c3935b1d859a
Ralph-Gordon Paul <gordon@rgpaul.com>
2019-08-23 12:07:41 +0200

    Refactor conanfile

--- conanfile.py
+++ conanfile.py
@@ -25,83 +25,63 @@ class LibreSSLConan(ConanFile):
         cmake.verbose = True
 
         if self.settings.os == "Android":
-            android_toolchain = os.environ["ANDROID_NDK_PATH"] + "/build/cmake/android.toolchain.cmake"
-
-            cmake.definitions["CMAKE_SYSTEM_NAME"] = "Android"
-            cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = android_toolchain
-            cmake.definitions["ANDROID_NDK"] = os.environ["ANDROID_NDK_PATH"]
-            cmake.definitions["ANDROID_ABI"] = tools.to_android_abi(self.settings.arch)
-            cmake.definitions["ANDROID_STL"] = self.options.android_stl_type
-            cmake.definitions["ANDROID_NATIVE_API_LEVEL"] = self.settings.os.api_level
-            cmake.definitions["ANDROID_TOOLCHAIN"] = "clang"
+            self.applyCmakeSettingsForAndroid(cmake)
 
         if self.settings.os == "iOS":
-            ios_toolchain = "cmake-modules/Toolchains/ios.toolchain.cmake"
-            cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = ios_toolchain
-            
-            # specify the ios minimum sdk version for crypto
-            tools.replace_in_file("%s/libressl-%s/crypto/CMakeLists.txt" % (self.source_folder, self.version),
-                        "if (BUILD_SHARED_LIBS)",
-                        """set_xcode_property (crypto IPHONEOS_DEPLOYMENT_TARGET "10.0")
-                        if (BUILD_SHARED_LIBS) """)
-
-            # specify the ios minimum sdk version for ssl
-            tools.replace_in_file("%s/libressl-%s/ssl/CMakeLists.txt" % (self.source_folder, self.version),
-                        "if (BUILD_SHARED_LIBS)",
-                        """set_xcode_property (ssl IPHONEOS_DEPLOYMENT_TARGET "10.0")
-                        if (BUILD_SHARED_LIBS) """)
-
-            # specify the ios minimum sdk version for tls
-            tools.replace_in_file("%s/libressl-%s/tls/CMakeLists.txt" % (self.source_folder, self.version),
-                        "if (BUILD_SHARED_LIBS)",
-                        """set_xcode_property (tls IPHONEOS_DEPLOYMENT_TARGET "10.0")
-                        if (BUILD_SHARED_LIBS) """)
-
-            tools.replace_in_file("%s/libressl-%s/CMakeLists.txt" % (self.source_folder, self.version),
-                        "project (LibreSSL C ASM)",
-                        """project (LibreSSL C ASM)
-                        include_directories(BEFORE "ios/include") """)
-
-            cmake.definitions["LIBRESSL_APPS"] = "OFF"
-            cmake.definitions["LIBRESSL_TESTS"] = "OFF"
-            if self.settings.arch == "x86" or self.settings.arch == "x86_64":
-                cmake.definitions["IOS_PLATFORM"] = "SIMULATOR"
-            else:
-                cmake.definitions["IOS_PLATFORM"] = "OS"
-
-            # define all architectures for ios fat library
-            if "arm" in self.settings.arch:
-                self.variants = ["armv7", "armv7s", "armv8"]
-            else:
-                self.variants = []
-
-            # apply build config for all defined architectures
-            if len(self.variants) > 0:
-                archs = ""
-                for i in range(0, len(self.variants)):
-                    if i == 0:
-                        archs = tools.to_apple_arch(self.variants[i])
-                    else:
-                        archs += ";" + tools.to_apple_arch(self.variants[i])
-                cmake.definitions["CMAKE_OSX_ARCHITECTURES"] = archs
-            else:
-                cmake.definitions["CMAKE_OSX_ARCHITECTURES"] = tools.to_apple_arch(self.settings.arch)
+            self.applyCmakeSettingsForiOS(cmake)
 
         if self.settings.os == "Macos":
-            cmake.definitions["CMAKE_OSX_ARCHITECTURES"] = tools.to_apple_arch(self.settings.arch)
+            self.applyCmakeSettingsFormacOS(cmake)
 
         cmake.configure(source_folder=library_folder)
         cmake.build()
         cmake.install()
 
-        lib_dir = os.path.join(self.package_folder,"lib")
+    def applyCmakeSettingsForAndroid(self, cmake):
+        android_toolchain = os.environ["ANDROID_NDK_PATH"] + "/build/cmake/android.toolchain.cmake"
+        cmake.definitions["CMAKE_SYSTEM_NAME"] = "Android"
+        cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = android_toolchain
+        cmake.definitions["ANDROID_NDK"] = os.environ["ANDROID_NDK_PATH"]
+        cmake.definitions["ANDROID_ABI"] = tools.to_android_abi(self.settings.arch)
+        cmake.definitions["ANDROID_STL"] = self.options.android_stl_type
+        cmake.definitions["ANDROID_NATIVE_API_LEVEL"] = self.settings.os.api_level
+        cmake.definitions["ANDROID_TOOLCHAIN"] = "clang"
 
-        # execute ranlib for all static universal libraries (required for fat libraries)
-        if self.settings.os == "iOS" and len(self.variants) > 0:
-            if self.options.shared == False:
-                for f in os.listdir(lib_dir):
-                    if f.endswith(".a") and os.path.isfile(os.path.join(lib_dir,f)) and not os.path.islink(os.path.join(lib_dir,f)):
-                        self.run("xcrun ranlib %s" % os.path.join(lib_dir,f))
+    def applyCmakeSettingsForiOS(self, cmake):
+        ios_toolchain = "cmake-modules/Toolchains/ios.toolchain.cmake"
+        cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = ios_toolchain
+        variants = []
+        
+        tools.replace_in_file("%s/libressl-%s/CMakeLists.txt" % (self.source_folder, self.version),
+                    "project (LibreSSL C ASM)",
+                    """project (LibreSSL C ASM)
+                    include_directories(BEFORE "ios/include") """)
+
+        cmake.definitions["LIBRESSL_APPS"] = "OFF"
+        cmake.definitions["LIBRESSL_TESTS"] = "OFF"
+        if self.settings.arch == "x86" or self.settings.arch == "x86_64":
+            cmake.definitions["IOS_PLATFORM"] = "SIMULATOR"
+        else:
+            cmake.definitions["IOS_PLATFORM"] = "OS"
+
+        # define all architectures for ios fat library
+        if "arm" in self.settings.arch:
+            variants = ["armv7", "armv7s", "armv8"]
+
+        # apply build config for all defined architectures
+        if len(variants) > 0:
+            archs = ""
+            for i in range(0, len(variants)):
+                if i == 0:
+                    archs = tools.to_apple_arch(variants[i])
+                else:
+                    archs += ";" + tools.to_apple_arch(variants[i])
+            cmake.definitions["CMAKE_OSX_ARCHITECTURES"] = archs
+        else:
+            cmake.definitions["CMAKE_OSX_ARCHITECTURES"] = tools.to_apple_arch(self.settings.arch)
+    
+    def applyCmakeSettingsFormacOS(self, cmake):
+        cmake.definitions["CMAKE_OSX_ARCHITECTURES"] = tools.to_apple_arch(self.settings.arch)
 
     def package(self):
         self.copy("*", dst="include", src='include')
